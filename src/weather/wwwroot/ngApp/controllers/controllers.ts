fa902af7965eed0091a10ea082f8345f93f528bd
Chance Meyer <c.j.m.fowlston@gmail.com>
2016-10-27 17:29:08 -0700

    second commit

--- src/weather/wwwroot/ngApp/controllers/controllers.ts
+++ src/weather/wwwroot/ngApp/controllers/controllers.ts
@@ -1,7 +1,23 @@
 namespace weather.Controllers {
 
+    const apiURL = "http://api.openweathermap.org/data/2.5/weather?q=";
+    const apiKey = "&appid=57ace6926428cf6c2373c1de010ca4d0";
+
     export class HomeController {
-        public message = 'Hello from the home page!';
+
+        public search;
+        public forecast;
+
+        fetch() {
+            if (this.search) {
+                console.log('searching.......');
+                this.$http.get(apiURL + this.search + apiKey)
+                    .then((results) => {
+                        this.forecast = results.data;
+                    });
+            }
+        }
+        constructor(private $http: ng.IHttpService) { }
     }
 
 
